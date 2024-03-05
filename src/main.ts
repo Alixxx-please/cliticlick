272dbbbde4d7f3415f6f9d8716b798c6ac8a10b7
Alixxx-please <user012120@gmail.com>
2024-03-05 04:51:17 +0100

    Base working

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ const toggle = document.querySelector('.toggle') as HTMLDivElement;
 const input: HTMLInputElement = document.getElementById("input") as HTMLInputElement;
 const button = document.getElementById('button') as HTMLDivElement;
 const audio = document.querySelector('audio')
+const top = document.querySelector('.top') as HTMLDivElement;
+const bottom = document.querySelector('.bottom') as HTMLDivElement;
+let on = false;
 let delay = 1000;
 input.value = delay.toString();
 
@@ -19,9 +22,20 @@ input?.addEventListener("input", (e) => {
 await listen(('sound'), (e) => {
   const msg = e.payload;
   if (msg === 'on') {
+    on = true;
     audio?.play();
     toggle.style.backgroundColor = 'green';
+    button?.classList.remove('stop');
+    if (top) top.style.backgroundColor = 'transparent';
+    if (bottom) bottom.style.backgroundColor = 'transparent';
+    button?.classList.add('paused');
+
   } else if (msg === 'off') {
+    on = false;
+    button?.classList.remove('paused');
+    if (top) top.style.backgroundColor = 'white';
+    if (bottom) bottom.style.backgroundColor = 'white';
+    button?.classList.add('stop');
     audio?.pause();
     if (audio) audio.currentTime = 0;
     toggle.style.backgroundColor = 'red';
@@ -38,27 +52,41 @@ await listen(('hover'), (e) => {
   if (msg === 'hover') toggle.style.backgroundColor = 'orange';
 });
 
-document.addEventListener('mouseover', () => {
+window.addEventListener('mouseover', () => {
   window.focus();
-  emit('focus');
-  button?.classList.remove('paused');
+  emit('focus')
+  if (on) {
+    button?.classList.remove('stop');
+    button?.classList.remove('paused');
+  }
 });
 
-document.addEventListener('mousemove', () => {
+window.addEventListener('mousemove', () => {
   window.focus();
-  emit('focus');
-  button?.classList.remove('paused');
+  emit('focus')
+  if (on) {
+    button?.classList.remove('stop');
+    button?.classList.remove('paused');
+  }
 });
 
-document.addEventListener('mouseenter', () => {
+window.addEventListener('mouseenter', () => {
   window.focus();
-  emit('focus');
-  button?.classList.remove('paused');
+  emit('focus')
+  if (on) {
+    button?.classList.remove('stop');
+    button?.classList.remove('paused');
+  }
 });
 
 document.addEventListener('mouseout', () => {
   emit('leave');
-  button?.classList.add('paused');
+  if (on) {
+    button?.classList.remove('stop');
+    button?.classList.add('paused');
+  } else {
+    button?.classList.add('stop');
+  };
 });
 
 const unlisten = await listen<string>('error', (e) => {
